963255d3e7c634aee425a7f197c231b53f9b3ace
Lonz8576 <Lonz8576@users.noreply.github.com>
2024-08-10 18:33:33 -0400

    rules modal
    shown and hidden w tab

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+const rulesBtn = document.querySelector(".rules-btn");
+const closeBtn = document.querySelector(".btn-close");
+const modalRules = document.querySelector(".modal");
+
+
+    rulesBtn.addEventListener("click", () => {
+        modalRules.classList.toggle ("modal-shown")
+    } );
+
+
+   
+    closeBtn.addEventListener('click', () => {
+        modalRules.classList.toggle("modal-shown")
+    });
+
