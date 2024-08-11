9cf194a631c3f3ec1ab07b15cb43f20a58bfe098
Lonz8576 <Lonz8576@users.noreply.github.com>
2024-08-11 19:04:12 -0400

    JS started for game

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ setTimeout(()=> {
 }, 500);
 
 
-
-
-
 const rulesBtn = document.querySelector(".rules-btn");
 const closeBtn = document.querySelector(".btn-close");
 const modalRules = document.querySelector(".modal");
@@ -21,4 +18,34 @@ const modalRules = document.querySelector(".modal");
     closeBtn.addEventListener('click', () => {
         modalRules.classList.toggle("modal-shown")
     });
+
+
+    const CHOICES = [
+        {
+            name:"paper",
+            beats: "rock"
+        },
+        {
+            name:"scissors",
+            beats: "paper"
+        },
+        {
+            name:"rock",
+            beats: "scissors"
+        }
+    ];
+    const choiceButtons = document.querySelectorAll(".choice-btn");
+    const gameDiv = document.querySelector(".game");
+    const resultDiv = document.querySelector(".results");
+    const pickResults = document.querySelectorAll(".results-result");
+
+    choiceButtons.forEach( button => {
+        button.addEventListener( "click", () => {
+            const choiceName = button.dataset.choice;
+            const choice = CHOICES.find(choice => choice.name === choiceName);
+            choose(choice);
+
+        })
+    })
+
 
